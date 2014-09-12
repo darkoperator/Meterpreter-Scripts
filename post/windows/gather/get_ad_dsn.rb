c495de7c65fa031054071de8eb9542519703755d
Carlos Perez <carlos_perez@darkoperator.com>
2014-09-12 03:10:03 -0400

    Module to get current host DSN

---
+++ post/windows/gather/get_ad_dsn.rb
@@ -0,0 +1,50 @@
+require 'msf/core'
+require 'rex'
+require 'msf/core/auxiliary/report'
+
+class Metasploit3 < Msf::Post
+  include Msf::Auxiliary::Report
+  include Msf::Post::Windows::Registry
+
+  def initialize(info = {})
+    super(update_info(
+          info,
+          'Name'          => 'Windows Gather AD Get Domain Distinguished Name',
+          'Description'   => %q{ This Module will get the Distinguished Name for
+                              the domain the target host is a member of.},
+          'License'       => BSD_LICENSE,
+          'Author'        => 'Carlos Perez <carlos_perez[at]darkoperator.com>',
+          'Platform'      => 'win',
+          'SessionTypes'  => 'meterpreter'
+      ))
+    deregister_options('MAX_SEARCH','DOMAIN')
+  end
+
+  # Run Method for when run command is issued
+  def run
+    print_status("Running module against #{sysinfo['Computer']}")
+    dom = get_dn
+    print_good dom
+  end
+
+  def get_dn
+    dn = nil
+    begin
+      subkey = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History'
+      v_name = 'DCName'
+      key_vals = registry_enumvals(subkey)
+      if key_vals.include?(v_name)
+        domain_dc = registry_getvaldata(subkey, v_name)
+        # lets parse the information
+        dom_info =  domain_dc.split('.')
+        dn = "DC=#{dom_info[1,dom_info.length].join(',DC=')}"
+      else
+        print_status 'Host is not part of a domain.'
+      end
+    rescue
+      print_error('Could not determine if the host is part of a domain.')
+      return nil
+    end
+    dn
+  end
+end
